d75c51a3adc4f3958bcfb48223991b2b472b6abc
Sarv <S_Chokkalingam@acs.org>
2022-09-01 19:17:18 +0530

    v1

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use('/abc', movies);
 
 
 app.get('/', (req,res) => {
-    res.send('Welcome to Daily Code Buffer in Heroku Auto Deployment!!');
+    res.send('Welcome to Salesforce in Heroku Auto Deployment updated asdgit qa!!');
 })
 
 
@@ -21,5 +21,5 @@ app.get('/', (req,res) => {
 
 
 
-const port = process.env.PORT || '5000';
+const port = process.env.PORT || '3001';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
